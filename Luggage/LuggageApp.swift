d2e92b5c15fa590c745f0896a961dffbcb0d238a
wandath <anya.schouvey@ecole-hexagone.com>
2023-06-13 10:26:37 +0200

    Initial Commit

---
+++ Luggage/LuggageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuggageApp.swift
+//  Luggage
+//
+//  Created by Anya SCHOUVEY on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LuggageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
