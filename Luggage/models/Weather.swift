eb0e283cc6dbf9718c9fc6509329aac281fd267b
wandath <anya.schouvey@ecole-hexagone.com>
2023-06-13 12:05:21 +0200

    update weather class

--- Luggage/models/Weather.swift
+++ Luggage/models/Weather.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import WeatherKit
 
 final class Weather {
+    var meteo: WeatherCondition
+    var travelId: UUID
     
+    init(meteo: WeatherCondition, travelId: UUID) {
+        self.meteo = meteo
+        self.travelId = travelId
+    }
 }
